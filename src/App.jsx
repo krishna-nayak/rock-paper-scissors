75c228d00d2e8d6dd5f295bb0e1e1b3190665dde
Krishna <nayak100krishna@gmail.com>
2023-09-29 20:37:10 +0530

    code completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import scissor from "./assets/images/icon-scissors.svg";
 import { useState } from "react";
 import Decisioin from "./Decisioin";
 
+import RuleImage from "./assets/images/image-rules.svg";
+
+import "./rules.css";
+
 const options = ["rock", "scissor", "paper"];
 // const optionImage = [rock, scissor, paper];
 
@@ -17,14 +21,20 @@ function App() {
   const [user, setUser] = useState(null);
   const [computer, setComputer] = useState(null);
   const [result, setResult] = useState(null);
+  const [showResult, setShowResult] = useState(false);
   const [score, setScore] = useState(0);
+  const [ruleModal, setRuleModal] = useState(false);
 
   const handleClick = (user) => {
     console.log(user);
     const computer = options[Math.floor(Math.random() * options.length)];
     setUser(user);
     setComputer(computer);
-    handlePlay(user, computer);
+    setResult(null);
+    setShowResult(false);
+    setTimeout(() => {
+      handlePlay(user, computer);
+    }, 3000);
   };
 
   const handlePlay = (user, computer) => {
@@ -42,9 +52,16 @@ function App() {
       } else {
         setScore(score - 1);
       }
+
+      setTimeout(() => {
+        setShowResult(true);
+      }, 1000);
     } else {
       console.log("Draw");
       setResult("Draw");
+      setTimeout(() => {
+        setShowResult(true);
+      }, 1000);
     }
   };
   const winnerDecide = (object, user, computer) => {
@@ -90,8 +107,13 @@ function App() {
           <h1>{score}</h1>
         </div>
       </header>
-      <main>
-        {user === null ? (
+      <main
+        className={user === null ? "animation" : "animation animation-begin"}
+      >
+        <span>Rock</span>
+        <span>Paper</span>
+        <span>Scissors</span>
+        {result === null ? (
           <Choices handleClick={handleClick} />
         ) : (
           <Decisioin
@@ -101,6 +123,7 @@ function App() {
             computer={computer}
             setResult={setResult}
             setUser={setUser}
+            showResult={showResult}
           />
         )}
 
@@ -108,12 +131,35 @@ function App() {
 
         {/* rules */}
         <div>
-          <button className="rule-btn">Rules</button>
+          <button onClick={() => setRuleModal(true)} className="rule-btn">
+            Rules
+          </button>
         </div>
       </main>
+      <RuleModal modal={ruleModal} handleModal={() => setRuleModal(false)} />
     </div>
   );
 }
+
+const RuleModal = ({ modal, handleModal }) => {
+  return (
+    modal && (
+      <div className="rule-modal">
+        <div className="rule-modal-content">
+          <div className="rule-modal-header">
+            <h1>Rules</h1>
+            <button onClick={handleModal} className="close-btn">
+              x
+            </button>
+          </div>
+          <div className="rule-modal-body">
+            <img src={RuleImage} alt="rule-image" />
+          </div>
+        </div>
+      </div>
+    )
+  );
+};
 
 const Choices = ({ handleClick }) => {
   return (
@@ -145,3 +191,63 @@ const Choices = ({ handleClick }) => {
 };
 
 export default App;
+
+/* 
+
+<section id="arena">
+          <div className="user-choice">
+            <div className="title">your picked</div>
+            <div className={user + " object winner"}>
+              <div>
+                <img src={objectData[user]} alt={user} />
+              </div>
+            </div>
+          </div>
+          <div className={result === null ? "hidden" : "result block"}>
+            <h1>{result}</h1>
+            <button onClick={() => setResult(null)}>Play Again</button>
+          </div>
+          <div className="computer-choice">
+            <div className="title">the House Picked</div>
+            <div className={computer + " object"}>
+              <div>
+                <img src={objectData[computer]} alt={computer} />
+              </div>
+            </div>
+          </div>
+        </section>
+
+
+        // other
+
+
+
+
+         <div className="wrapper">
+          <div className="rock-position">
+            <div onClick={() => handleClick("rock")} className="rock object">
+              <div>
+                <img src={rock} alt="rock" />
+              </div>
+            </div>
+          </div>
+          <div className="paper-position">
+            <div onClick={() => handleClick("paper")} className="paper object">
+              <div>
+                <img src={paper} alt="paper" />
+              </div>
+            </div>
+          </div>
+
+          <div className="scissor-position">
+            <div
+              onClick={() => handleClick("scissor")}
+              className="scissor object"
+            >
+              <div>
+                <img src={scissor} alt="scissor" />
+              </div>
+            </div>
+          </div>
+        </div>
+*/
--- src/Decisioin.jsx
+++ src/Decisioin.jsx
@@ -1,3 +1,5 @@
+import useSize from "./useSize";
+
 export default function Decisioin({
   user,
   objectData,
@@ -5,36 +7,77 @@ export default function Decisioin({
   computer,
   setResult,
   setUser,
+  showResult,
 }) {
+  console.log({
+    user,
+    objectData,
+    result,
+    computer,
+    setResult,
+    setUser,
+    showResult,
+  });
+  const size = useSize();
   return (
-    <section id="arena">
-      <div className="user-choice">
-        <div className="title">your picked</div>
-        <div className={user + " object winner"}>
-          <div>
-            <img src={objectData[user]} alt={user} />
+    <>
+      <section id="arena">
+        <div className={"user-choice"}>
+          <div className="title">your picked</div>
+          <div
+            // className="winner"
+            className={result === "Win" ? "winner" : ""}
+          >
+            <div className={user + " object "}>
+              <div>
+                <img src={objectData[user]} alt={user} />
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      <div className={result === null ? "hidden" : "result block"}>
-        <h1>{result}</h1>
-        <button
-          onClick={() => {
-            setResult(null);
-            setUser(null);
-          }}
-        >
-          Play Again
-        </button>
-      </div>
-      <div className="computer-choice">
-        <div className="title">the House Picked</div>
-        <div className={computer + " object"}>
-          <div>
-            <img src={objectData[computer]} alt={computer} />
+        {size.width >= 600 ? (
+          <div className={showResult ? "result block" : "hidden"}>
+            <h1>{result}</h1>
+            <button
+              className="btn btn-primary"
+              onClick={() => {
+                setResult(null);
+                setUser(null);
+              }}
+            >
+              Play Again
+            </button>
+          </div>
+        ) : null}
+
+        <div className="computer-choice">
+          <div className="title">the House Picked</div>
+          <div
+            // className={"winner"}
+            className={result === "Loose" ? "winner" : ""}
+          >
+            <div className={computer + " object"}>
+              <div>
+                <img src={objectData[computer]} alt={computer} />
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+      {size.width < 600 ? (
+        <div className={showResult ? "result block text-center" : "hidden"}>
+          <h1>{result}</h1>
+          <button
+            className="btn btn-primary"
+            onClick={() => {
+              setResult(null);
+              setUser(null);
+            }}
+          >
+            Play Again
+          </button>
+        </div>
+      ) : null}
+    </>
   );
 }
